a8f6a87ff6986124c74383f8a090f1e361caa6e0
Alibek Isomov <99270031+AlibekIsomov@users.noreply.github.com>
2024-12-30 22:45:20 +0500

    library changed(4 some reasons it didn't work)

--- app/Models/Move.php
+++ app/Models/Move.php
@@ -2,22 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Move extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['game_id', 'user_id', 'position'];
-
-    public function game()
-    {
-        return $this->belongsTo(Game::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 }
